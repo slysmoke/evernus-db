31b70a3f0d086fa3d427d0f4af5d9de8c3b98f33
slysmoke <dmitri.merzin@gmail.com>
2024-06-12 03:49:10 +0300

    add market group parser

--- db_action.py
+++ db_action.py
@@ -12,9 +12,7 @@ def download_reference_data():
     url = "https://data.everef.net/reference-data/reference-data-latest.tar.xz"
     r = requests.get(url, stream=True)
     if r.status_code != 200:
-        raise Exception(
-            f"HTTP error {r.status_code} when getting reference data"
-        )
+        raise Exception(f"HTTP error {r.status_code} when getting reference data")
     with open("reference-data-latest.tar.xz", "wb") as f:
         for chunk in r.iter_content(chunk_size=1024 * 1024):
             if chunk:
@@ -24,6 +22,13 @@ def download_reference_data():
 def read_types_json_from_reference_data():
     with tarfile.open("reference-data-latest.tar.xz", "r:xz") as f:
         file = f.extractfile("types.json")
+        data = json.load(file)
+        return data
+
+
+def read_market_groups_json_from_reference_data():
+    with tarfile.open("reference-data-latest.tar.xz", "r:xz") as f:
+        file = f.extractfile("market_groups.json")
         data = json.load(file)
         return data
 
@@ -39,6 +44,7 @@ def check_md5sum(data):
 def droptable():
     cursor = db.cursor()
     cursor.execute("DROP TABLE IF EXISTS invTypes")
+    cursor.execute("DROP TABLE IF EXISTS invMarketGroups")
     db.commit()
 
 
@@ -66,8 +72,49 @@ def create_table():
         graphicID INTEGER
     )
     """
+    query2 = """
+    CREATE TABLE "invMarketGroups" (
+	"marketGroupID"	INTEGER NOT NULL,
+	"parentGroupID"	INTEGER DEFAULT NULL,
+	"marketGroupName"	VARCHAR(100) DEFAULT NULL,
+	"description"	VARCHAR(3000) DEFAULT NULL,
+	"iconID"	INTEGER DEFAULT NULL,
+	"hasTypes"	INTEGER DEFAULT NULL,
+	PRIMARY KEY("marketGroupID")
+)
+    """
+
     cursor = db.cursor()
     cursor.execute(query)
+    cursor.execute(query2)
+    db.commit()
+
+
+def insert_into_inv_market_groups(data):
+    query = """
+    INSERT INTO invMarketGroups (
+        marketGroupID,
+        parentGroupID,
+        marketGroupName,
+        description,
+        iconID,
+        hasTypes
+    ) VALUES (?, ?, ?, ?, ?, ?)
+    """
+    cursor = db.cursor()
+    for d in data:
+        cursor.execute(
+            query,
+            (
+                d.get("market_group_id"),
+                d.get("parent_group_id"),
+                d.get("name", {}).get("en"),
+                d.get("description", {}).get("en"),
+                d.get("icon_id"),
+                d.get("has_types"),
+            ),
+        )
+
     db.commit()
 
 
@@ -120,6 +167,7 @@ def main():
     print("Download reference data")
     download_reference_data()
     data = read_types_json_from_reference_data()
+    market_group_data = read_market_groups_json_from_reference_data()
     md5sum = check_md5sum(data)
 
     with open(r"latest_version.json", "r") as f:
@@ -132,6 +180,8 @@ def main():
         create_table()
         print("Insert types into db")
         insert_into_inv_types(data.values())
+        print("Insert market groups into db")
+        insert_into_inv_market_groups(market_group_data.values())
         print("Update md5sum in latest_version.json")
         latest_version["sdeVersion"] = md5sum
         with open(r"latest_version.json", "w") as f:
